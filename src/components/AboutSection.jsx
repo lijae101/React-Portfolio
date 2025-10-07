4c7a551446b847f83937fd8cf6a26d419e03e8e3
Liam Janicke <liam.jaenicke@gmail.com>
2025-10-07 10:38:06 -0700

    Added downloadable resume

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -28,7 +28,7 @@ export const AboutSection = () => {
                         <a href="#contact" className="cosmic-button">
                             Get In Touch
                         </a>
-                        <a href="" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
+                        <a href="" download="Liam_Janicke.pdf" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
                             Download Resume
                         </a>
                     </div>
